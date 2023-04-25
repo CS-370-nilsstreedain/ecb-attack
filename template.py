cba0a33db3c8011571374e50d957ecbf3977dd28
Nils Streedain <streedan@oregonstate.edu>
2023-04-25 00:53:23 +0000

    Completed ECB Attack 2

--- template.py
+++ template.py
@@ -129,6 +129,7 @@ def create_file_for_flag_2():
     copied_blocks_int = copy.deepcopy(blocks_int)
 
     # XXX: Your code here; transform the blocks here
+    copied_blocks_int[6] = copied_blocks_int[0]
 
     # in case you used blocks_int
     bytestring = convert_int_blocks_to_bytestring(copied_blocks_int)
